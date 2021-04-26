dd0a240ced4129a1eb2bcb38c63cc729752d398d
planeset94 <planeset94@gmail.com>
2021-04-26 16:05:52 +0200

    Adding max function and coditions

--- dadi/asset/JS/dadi.js
+++ dadi/asset/JS/dadi.js
@@ -1,4 +1,4 @@
-// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. 
 
 // DICHIARO LE VARIABILI 
 var user_num;
@@ -7,7 +7,23 @@ var computer_num;
 // ASSOCIO UN VALORE CASUALE A CIASCUNA VARIABILE
 
 user_num=Math.random()*6;
-console.log(user_num);
+console.log("user "+user_num);
 
 computer_num=Math.random()*6;
-console.log(computer_num);
+console.log("computer "+computer_num);
+
+// Stabilire il vincitore in base a chi fa il punteggio più alto.
+
+var risultato=Math.max(user_num, computer_num);
+console.log("risultato "+risultato);
+
+if (risultato==computer_num){
+    document.getElementById('winner').innerHTML="The Computer wins with a score of "+risultato.toFixed(3);
+}
+else{
+    document.getElementById('winner').innerHTML="The User wins with a score of "+risultato.toFixed(3);
+
+    }
+
+
+
