eefcbd9958b734e1778a48d56e1d28fcbe5f9985
planeset94 <planeset94@gmail.com>
2021-04-26 15:44:00 +0200

    variables creation & user random number

---
+++ dadi/asset/JS/dadi.js
@@ -0,0 +1,10 @@
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// DICHIARO LE VARIABILI 
+var user_num;
+var computer_num;
+
+// ASSOCIO UN VALORE CASUALE A CIASCUNA VARIABILE
+
+user_num=Math.random();
+console.log(user_num*100);
